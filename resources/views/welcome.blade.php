0d5fa8c7f2e1ba4ae07af1c67f36c889aba72528
Luciano José de Araújo <luciano.9912@gmail.com>
2020-07-06 07:54:05 -0300

    Mudei o titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel DU
+                    Curso de Laravel
                 </div>
 
                 <div class="links">
